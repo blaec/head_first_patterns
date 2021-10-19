c6adeea5bb2224d820de4ac2200d22e3e94a9322
blaec <odeskonst@yandex.ru>
2021-10-19 21:28:44 +0300

    factory | simple pizza factory

---
+++ src/main/java/patterns/factory/pizzas/SimplePizzaFactory.java
@@ -0,0 +1,21 @@
+package patterns.factory.pizzas;
+
+import patterns.factory.pizzas.objects.*;
+
+public class SimplePizzaFactory {
+
+	public Pizza createPizza(String type) {
+		Pizza pizza = null;
+
+		if (type.equals("cheese")) {
+			pizza = new CheesePizza();
+		} else if (type.equals("pepperoni")) {
+			pizza = new PepperoniPizza();
+		} else if (type.equals("clam")) {
+			pizza = new ClamPizza();
+		} else if (type.equals("veggie")) {
+			pizza = new VeggiePizza();
+		}
+		return pizza;
+	}
+}
