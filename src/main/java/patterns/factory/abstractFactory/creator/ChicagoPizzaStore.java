329bc2a0ad6f4acc02bef7f746bf4d8a472c27e7
blaec <odeskonst@yandex.ru>
2021-10-22 21:32:49 +0300

    factory | abstract factory - refactoring

--- src/main/java/patterns/factory/abstractFactory/creator/ChicagoPizzaStore.java
+++ src/main/java/patterns/factory/abstractFactory/creator/ChicagoPizzaStore.java
@@ -9,22 +9,24 @@ public class ChicagoPizzaStore extends PizzaStore {
 	@Override
 	protected Pizza createPizza(String item) {
 		Pizza pizza = null;
-		PizzaIngredientFactory ingredientFactory =
-		new ChicagoPizzaIngredientFactory();
+		PizzaIngredientFactory ingredientFactory = new ChicagoPizzaIngredientFactory();
 
 		switch (item) {
 			case "cheese":
 				pizza = new CheesePizza(ingredientFactory);
 				pizza.setName("Chicago Style Cheese Pizza");
 				break;
+
 			case "veggie":
 				pizza = new VeggiePizza(ingredientFactory);
 				pizza.setName("Chicago Style Veggie Pizza");
 				break;
+
 			case "clam":
 				pizza = new ClamPizza(ingredientFactory);
 				pizza.setName("Chicago Style Clam Pizza");
 				break;
+
 			case "pepperoni":
 				pizza = new PepperoniPizza(ingredientFactory);
 				pizza.setName("Chicago Style Pepperoni Pizza");
