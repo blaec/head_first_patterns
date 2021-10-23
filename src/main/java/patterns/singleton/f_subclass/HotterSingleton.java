4291d50f6de460a0df46bc78a515340b85ed28cd
blaec <odeskonst@yandex.ru>
2021-10-23 23:16:07 +0300

    singleton | subclass

---
+++ src/main/java/patterns/singleton/f_subclass/HotterSingleton.java
@@ -0,0 +1,11 @@
+package patterns.singleton.f_subclass;
+
+public class HotterSingleton extends Singleton {
+	// useful instance variables here
+ 
+	private HotterSingleton() {
+		super();
+	}
+ 
+	// useful methods here
+}
---
+++ src/main/java/patterns/singleton/f_subclass/SingletonTestDrive.java
@@ -0,0 +1,10 @@
+package patterns.singleton.f_subclass;
+
+public class SingletonTestDrive {
+	public static void main(String[] args) {
+		Singleton foo = CoolerSingleton.getInstance();
+		Singleton bar = HotterSingleton.getInstance();
+		System.out.println(foo);
+		System.out.println(bar);
+ 	}
+}
