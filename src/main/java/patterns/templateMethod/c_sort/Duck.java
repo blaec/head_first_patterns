7065d750d077a8664fd97fd85f3cf1da76d5fa9a
blaec <odeskonst@yandex.ru>
2021-10-30 20:38:51 +0300

    template method | sort - refactoring

--- src/main/java/patterns/templateMethod/c_sort/Duck.java
+++ src/main/java/patterns/templateMethod/c_sort/Duck.java
@@ -1,27 +1,19 @@
 package patterns.templateMethod.c_sort;
 
 public class Duck implements Comparable<Duck> {
-	String name;
-	int weight;
-  
-	public Duck(String name, int weight) {
-		this.name = name;
-		this.weight = weight;
-	}
- 
-	public String toString() {
-		return name + " weighs " + weight;
-	}
-  
-	public int compareTo(Duck otherDuck) {
- 
-  
-		if (this.weight < otherDuck.weight) {
-			return -1;
-		} else if (this.weight == otherDuck.weight) {
-			return 0;
-		} else { // this.weight > otherDuck.weight
-			return 1;
-		}
-	}
+    String name;
+    int weight;
+
+    public Duck(String name, int weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public String toString() {
+        return name + " weighs " + weight;
+    }
+
+    public int compareTo(Duck otherDuck) {
+        return Integer.compare(this.weight, otherDuck.weight);
+    }
 }
