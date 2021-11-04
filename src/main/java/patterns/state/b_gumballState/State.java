744cda51b98343f3e69cbde0f9493f9170894c6a
blaec <odeskonst@yandex.ru>
2021-11-04 22:22:05 +0200

    state | gumball state - formatting

--- src/main/java/patterns/state/b_gumballState/State.java
+++ src/main/java/patterns/state/b_gumballState/State.java
@@ -1,11 +1,14 @@
 package patterns.state.b_gumballState;
 
 public interface State {
- 
-	public void insertQuarter();
-	public void ejectQuarter();
-	public void turnCrank();
-	public void dispense();
-	
-	public void refill();
+
+    void insertQuarter();
+
+    void ejectQuarter();
+
+    void turnCrank();
+
+    void dispense();
+
+    void refill();
 }
